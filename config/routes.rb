3f4a413603ffad10af004fb555efa4aa56dfaf06
kpvarma <krishna@rightsolutions.ae>
2019-12-16 09:34:01 +0400

    fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :admin_users
+  devise_for :client_users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "home#index"
+  
 end
