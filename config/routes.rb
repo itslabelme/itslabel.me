b567947e6725dba51ca9617a1c01157671079404
kpvarma <krishna@rightsolutions.ae>
2019-12-17 10:01:28 +0400

    fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   # devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register', edit: 'settings' }
   devise_for :client_users, path: "users", path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register', edit: 'settings' }
-  devise_for :admin_users, path_names: { sign_in: 'a_login', sign_out: 'alogout', sign_up: 'a_register', edit: 'a_settings' }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :admin_users, path: "admin", skip: [:registrations], path_names: { sign_in: 'login', sign_out: 'logout', edit: 'settings' }
+
 
   root to: "home#index"
   get '/home', to: 'home#index', as: 'home'
