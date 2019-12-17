09437172ca3939818436e79263e8d52055e34196
sanoop <sanoop.snair@gmail.com>
2019-12-17 16:11:05 +0530

    update login UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,17 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :client_user
+      new_client_user_session_path
+    elsif resource_or_scope == :admin
+      new_admin_user_session_path
+    else
+      root_path
+    end
+  end
   
   # def layout
   #   # only turn it off for login pages:
