3545b7075bb25b6ae424c7cfb712a773fa291880
sanoop <sanoop.snair@gmail.com>
2020-01-18 16:01:19 +0530

    update changes

--- db/migrate/20200118091614_create_documents.rb
+++ db/migrate/20200118091614_create_documents.rb
@@ -1,8 +1,11 @@
 class CreateDocuments < ActiveRecord::Migration[5.2]
   def change
     create_table :documents do |t|
-
-      t.timestamps
+      t.references :user, index: true
+      
+      t.string :name, limit: 256, null: false
+      t.string :type, limit: 128 # should be 'table_based' or 'template_based' 
+      t.timestamps null: false
     end
   end
 end
--- db/migrate/20200118091731_create_tags.rb
+++ db/migrate/20200118091731_create_tags.rb
@@ -2,7 +2,14 @@ class CreateTags < ActiveRecord::Migration[5.2]
   def change
     create_table :tags do |t|
 
-      t.timestamps
+      t.string :label_eng, limit: 256, null: false
+      t.string :label_french, limit: 256
+      t.string :label_arbi, limit: 256
+      t.string :label_russian, limit: 256
+      t.string :ingradiant_value, limit: 256
+      t.string :daily_value, limit: 256
+      t.timestamps null: false
+
     end
   end
 end
