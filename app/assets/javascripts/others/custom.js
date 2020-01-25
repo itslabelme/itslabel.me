fa5304e75219e4c095b3e50ca9293af6b14da5fd
sanoop <sanoop.snair@gmail.com>
2020-01-25 21:29:47 +0530

    update changes

--- app/assets/javascripts/others/custom.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-  //CKEditor
-
-  alert("ff")
-  CKEDITOR.replace('ckeditor');
-  CKEDITOR.config.height = 300;
-  //CKEDITOR.config.removeButtons = 'Source';
-  CKEDITOR.config.removeButtons = 'Underline,Subscript,Superscript';
-});
