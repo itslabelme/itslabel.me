25e0a16c91844f101721a4f006528e8d49476092
sanoop <sanoop.snair@gmail.com>
2020-01-26 08:27:22 +0530

    remove pry

--- app/models/concerns/itslabel/callbacks/document_callbacks.rb
+++ app/models/concerns/itslabel/callbacks/document_callbacks.rb
@@ -5,7 +5,6 @@ module Itslabel::Callbacks::DocumentCallbacks
   def translate
     self.skip_callback = true
     translated_html = Translation.translate_html(self.input_html_source, input_language: self.input_language, output_language: self.output_1_language)
-    binding.pry
     self.update_attribute(:output_html_source, translated_html)
     # if self.new_record?
     #   self.output_html_source = Translation.translate_html(self.input_html_source, input_language: self.input_language, output_language: self.output_1_language)
