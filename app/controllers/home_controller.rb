0738d8b6ed3ed794734a025f44a81f03c471134c
kpvarma <krishna@rightsolutions.ae>
2020-01-27 10:25:41 +0530

    fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,10 @@
- class HomeController < ActionController::Base
-
-   
-
-    def index
-
-      @page_title = "Home | User"
-      @nav = 'user/home'
-      
-    end
-
+class HomeController < ActionController::Base
+  
+  layout 'login'
+  
+  def index
+    @page_title = "Home | User"
+    @nav = 'user/home'
   end
+
+end
