207ec6db9501b18f69788bfabb99b91ae5066816
kpvarma <krishna@rightsolutions.ae>
2020-02-09 08:01:59 +0400

    fixes

--- app/controllers/admin/client_users_controller.rb
+++ app/controllers/admin/client_users_controller.rb
@@ -2,56 +2,48 @@ module Admin
   class ClientUsersController < Admin::BaseController
 
     before_action :authenticate_admin_user!
-   
     before_action :get_client_user, except:[:index]
+
     def index
-      @page_title = "Client Users"
+      @page_title = "Registrations"
       @nav = "admin/registrations"
     
       get_collection 
       new_client_user
     end
     
-    
     def show
-       get_client_user
+      get_client_user
     end
-    def edit
-     
-    end
+
     private 
+
     def new_client_user
       @client_user = ClientUser.new
     end
+
     def get_client_user
       @client_user = ClientUser.find_by(id: params[:id])
     end
   
     def get_collection
-           
       @relation = ClientUser.where("")
 
       apply_filters
 
       @client_users = @relation.order(@order_by).page(@current_page).per(@per_page)
     end
+
     def apply_filters
       @query = params[:q]
       @relation = @relation.search(@query) if @query && !@query.blank?
-      @condition=" 1=1 ";
-      if params[:country].present?
-        @condition += " AND country='#{params[:country]}'" if params.key?(:country)
-      end
-      if params[:mobile_number].present?
-        @condition += " AND mobile_number LIKE '%#{params[:mobile_number]}%'" if params.key?(:mobile_number)
-      end
-      if params[:name].present?
-        @condition += " AND (first_name LIKE '%#{params[:name]}%' OR last_name LIKE '%#{params[:name]}%' OR email LIKE '%#{params[:name]}%' ) " if params.key?(:name)
-      end 
-      @relation = @relation.advsearch(@condition) if @condition && !@condition.blank?
+      @relation = @relation.filter_by_country(params[:filters].try(:[], :country))
+      @relation = @relation.search_only_mobile_number(params[:filters].try(:[], :mobile_number))
     end
+
     def client_user_params
-      params.require(:clinet_user).permit(:id, :email, :first_name, :last_name, :mobile_number)
+      params.require(:client_user).permit(:id, :email, :first_name, :last_name, :mobile_number)
     end
+
   end
 end
--- app/models/concerns/itslabel/scopes/client_user_scopes.rb
+++ app/models/concerns/itslabel/scopes/client_user_scopes.rb
@@ -10,11 +10,18 @@ module Itslabel::Scopes::ClientUserScopes
       LOWER(client_users.last_name) LIKE LOWER('%#{query}%') OR\
       LOWER(client_users.mobile_number) LIKE LOWER('%#{query}%') OR\
       LOWER(client_users.email) LIKE LOWER('%#{query}%') OR\
+      LOWER(client_users.organisation) LIKE LOWER('%#{query}%') OR\
       LOWER(client_users.country) LIKE ('%#{query}%')
     ")}
-     scope :advsearch, lambda {|query| where("
-      #{query}
+    
+    scope :filter_by_country, lambda {|country| country.to_s.blank? ? where("") : where("
+      LOWER(client_users.country) = LOWER('#{ISO3166::Country[country].translations[I18n.locale.to_s]}')
     ")}
+
+    scope :search_only_mobile_number, lambda {|mobile_number| mobile_number.to_s.blank? ? where("") : where("
+      LOWER(client_users.mobile_number) LIKE LOWER('%#{mobile_number}%')
+    ")}
+
     scope :upcoming, lambda { where("client_users.created_at >= ?", Time.now) }
     scope :past, lambda { where("client_users.created_at < ?", Time.now) }
 
