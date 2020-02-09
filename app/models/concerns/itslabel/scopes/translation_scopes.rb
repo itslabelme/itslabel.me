e03adf3195701558c35e92dd6f6c61da0c7e277c
vanirankar <vanirankar@gmail.com>
2020-02-09 12:14:21 +0530

    view and filter optimization

--- app/models/concerns/itslabel/scopes/translation_scopes.rb
+++ app/models/concerns/itslabel/scopes/translation_scopes.rb
@@ -15,9 +15,21 @@ module Itslabel::Scopes::TranslationScopes
     
 
     ")}
-     scope :advsearch, lambda {|query| where("
-      #{query}
-
+ 
+  scope :search_only_input_phrase, lambda {|input_phrase| input_phrase.to_s.blank? ? where("") : where("
+      LOWER(translations.input_phrase) LIKE LOWER('%#{input_phrase}%')
+    ")}
+   scope :search_only_output_phrase, lambda {|output_phrase| output_phrase.to_s.blank? ? where("") : where("
+      LOWER(translations.output_phrase) LIKE LOWER('%#{output_phrase}%')
+    ")}
+    scope :search_only_input_language, lambda {|input_language| input_language.to_s.blank? ? where("") : where("
+      LOWER(translations.input_language) LIKE LOWER('%#{input_language}%')
+    ")}
+scope :search_only_output_language, lambda {|output_language| output_language.to_s.blank? ? where("") : where("
+      LOWER(translations.output_language) LIKE LOWER('%#{output_language}%')
+    ")}
+scope :search_only_status, lambda {|status| status.to_s.blank? ? where("") : where("
+      LOWER(translations.status) LIKE LOWER('%#{status}%')
     ")}
     scope :upcoming, lambda { where("translations.created_at >= ?", Time.now) }
     scope :past, lambda { where("translations.created_at < ?", Time.now) }
