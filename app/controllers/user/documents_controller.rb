0911aa98d03e4d85cac5131a4849cfa78728ee67
kpvarma <krishna@rightsolutions.ae>
2020-02-11 10:31:17 +0400

    fixes

--- app/controllers/user/documents_controller.rb
+++ app/controllers/user/documents_controller.rb
@@ -13,7 +13,6 @@ module User
     private
 
     def get_collection
-      @order_by = "created_at DESC" unless @order_by
       @relation = TemplateDocument.where("")
 
       apply_filters
@@ -26,14 +25,26 @@ module User
       @relation = @relation.search(@query) if @query && !@query.blank?
 
       # Filter by Status
-      @relation = @relation.status(params[:status].upcase) if params[:status]
+      @relation = @relation.status(params[:status].to_s.strip.upcase) unless params[:status].to_s.strip.blank?
 
       # Filter by Favorite
       @relation = @relation.only_favorites if params[:favorite]
       
-      # @relation = @relation.search_only_title(params[:filters].try(:[], :title))
-      # @relation = @relation.search_only_input_language(params[:filters].try(:[], :input_language))
-      # @relation = @relation.search_only_output_language(params[:filters].try(:[], :output_language))
+      f_title = params[:f_title]
+      @relation = @relation.search_only_title(f_title) if f_title
+
+      f_input_language = params[:f_ilang]
+      @relation = @relation.search_only_input_language(f_input_language) if f_input_language
+
+      f_output_language = params[:f_olang]
+      @relation = @relation.search_only_output_language(f_output_language) if f_output_language
+
+      # Recent
+      @relation = @relation.recent
+
+      # Set Default Order
+      @order_by = "created_at DESC" unless @order_by
+      
     end
 
   end
--- app/models/concerns/itslabel/scopes/template_document_scopes.rb
+++ app/models/concerns/itslabel/scopes/template_document_scopes.rb
@@ -5,33 +5,29 @@ module Itslabel::Scopes::TemplateDocumentScopes
   included do
 
     # Scopes
-    scope :search, lambda {|query| where("
+    scope :search, lambda {|query| query.to_s.strip.blank? ? where("") : where("
       LOWER(template_documents.title) LIKE LOWER('%#{query}%') OR\
       LOWER(template_documents.input_language) LIKE LOWER('%#{query}%') OR\
       LOWER(template_documents.output_language) LIKE LOWER('%#{query}%') OR\
       LOWER(template_documents.status) LIKE LOWER('%#{query}%')
     ")}
 
-    scope :search_only_title, lambda {|title| title.to_s.blank? ? where("") : where("
+    scope :search_only_title, lambda {|title| title.to_s.strip.blank? ? where("") : where("
       LOWER(template_documents.title) LIKE LOWER('%#{title}%')
     ")}
 
-    scope :search_only_output_phrase, lambda {|output_phrase| output_phrase.to_s.blank? ? where("") : where("
-      LOWER(template_documents.output_phrase) LIKE LOWER('%#{output_phrase}%')
+    scope :search_only_input_language, lambda {|lang| lang.to_s.strip.blank? ? where("") : where("
+      LOWER(template_documents.input_language) LIKE LOWER('%#{lang}%')
     ")}
 
-    scope :search_only_input_language, lambda {|input_language| input_language.to_s.blank? ? where("") : where("
-      LOWER(template_documents.input_language) LIKE LOWER('%#{input_language}%')
+    scope :search_only_output_language, lambda {|lang| lang.to_s.strip.blank? ? where("") : where("
+      LOWER(template_documents.output_language) LIKE LOWER('%#{lang}%')
     ")}
-
-    # scope :search_only_output_language, lambda {|output_language| output_language.to_s.blank? ? where("") : where("
-    #   LOWER(template_documents.output_language) LIKE LOWER('%#{output_language}%')
-    # ")}
 
     scope :only_favorites, lambda { where(:favorite, true) }
     scope :not_favorites, lambda { where(:favorite, false) }
 
-    scope :recent, lambda { where("template_documents.created_at >= ?", 1.month.ago) }
+    scope :recent, lambda { where(created_at: 1.month.ago..Time.now).or(TemplateDocument.where(updated_at: 1.month.ago..Time.now)) }
 
   end
   
