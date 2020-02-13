d3b34e9d69fc50e2c3eacba0c36117b82af7f7a0
vanirankar <vanirankar@gmail.com>
2020-02-13 10:40:09 +0530

    registarion page modification

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -17,3 +17,28 @@ $(document).ready(function() {
         $('#mycheckboxdiv5').toggle();
     });
 });
+ 
+  
+  $(document).on("change", "#country_id", function(){
+  var country = $(this).val();
+
+  $.ajax({
+    url: "/user/get_time_zone/",
+    method: "GET",
+    dataType: "json",
+    data: {id: country},
+    error: function (xhr, status, error) {
+      console.error('AJAX Error: ' + status + error);
+    },
+    success: function (response) {
+      console.log(response);
+      var time_zones = response["time_zones"];
+      $("#time_zone").empty();
+
+      $("#time_zone").append('<option>Select city</option>');
+      for(var i=0; i< time_zones.length; i++){
+        $("#time_zone").append('<option value="' + time_zones[i]["id"] + '">' + time_zones[i]["name"] + '</option>');
+      }
+    }
+  });
+});
