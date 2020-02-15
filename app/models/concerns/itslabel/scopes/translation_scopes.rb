390268d8c8b194430df9f93018780d5047599f9e
kpvarma <krishna@rightsolutions.ae>
2020-02-15 12:30:10 +0530

    fixes

--- app/models/concerns/itslabel/scopes/translation_scopes.rb
+++ app/models/concerns/itslabel/scopes/translation_scopes.rb
@@ -6,31 +6,29 @@ module Itslabel::Scopes::TranslationScopes
 
     # Scopes
     scope :search, lambda {|query| where("
-      LOWER(translations.input_phrase) LIKE LOWER('%#{query}%') OR\
+      LOWER(translations.input_phrase) LIKE LOWER(?) OR\
     
-      LOWER(translations.input_language) LIKE LOWER('%#{query}%') OR\
+      LOWER(translations.input_language) LIKE LOWER(?) OR\
 
-      LOWER(translations.output_phrase) LIKE LOWER('%#{query}%') OR\
-      LOWER(translations.output_language) LIKE LOWER('%#{query}%') 
-    
-
-    ")}
+      LOWER(translations.output_phrase) LIKE LOWER(?) OR\
+      LOWER(translations.output_language) LIKE LOWER(?) 
+    ", "%#{query}%", "%#{query}%", "%#{query}%", "%#{query}%")}
  
-  scope :search_only_input_phrase, lambda {|input_phrase| input_phrase.to_s.blank? ? where("") : where("
-      LOWER(translations.input_phrase) LIKE LOWER('%#{input_phrase}%')
-    ")}
-   scope :search_only_output_phrase, lambda {|output_phrase| output_phrase.to_s.blank? ? where("") : where("
-      LOWER(translations.output_phrase) LIKE LOWER('%#{output_phrase}%')
-    ")}
+    scope :search_only_input_phrase, lambda {|input_phrase| input_phrase.to_s.blank? ? where("") : where("
+      LOWER(translations.input_phrase) LIKE LOWER(?)", "%#{input_phrase}%")}
+
+    scope :search_only_output_phrase, lambda {|output_phrase| output_phrase.to_s.blank? ? where("") : where("
+      LOWER(translations.output_phrase) LIKE LOWER(?)", output_phrase)}
+
     scope :search_only_input_language, lambda {|input_language| input_language.to_s.blank? ? where("") : where("
-      LOWER(translations.input_language) LIKE LOWER('%#{input_language}%')
-    ")}
-scope :search_only_output_language, lambda {|output_language| output_language.to_s.blank? ? where("") : where("
-      LOWER(translations.output_language) LIKE LOWER('%#{output_language}%')
-    ")}
-scope :search_only_status, lambda {|status| status.to_s.blank? ? where("") : where("
-      LOWER(translations.status) LIKE LOWER('%#{status}%')
-    ")}
+      LOWER(translations.input_language) = LOWER(?)", input_language)}
+
+    scope :search_only_output_language, lambda {|output_language| output_language.to_s.blank? ? where("") : where("
+      LOWER(translations.output_language) = LOWER(?)", output_language)}
+
+    scope :search_only_status, lambda {|status| status.to_s.blank? ? where("") : where("
+      LOWER(translations.status) = LOWER(?)", status)}
+
     scope :upcoming, lambda { where("translations.created_at >= ?", Time.now) }
     scope :past, lambda { where("translations.created_at < ?", Time.now) }
 
