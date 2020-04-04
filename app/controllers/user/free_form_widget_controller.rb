001150b05e4cf227144c94e168dab72d9d55bd33
kpvarma <krishna@rightsolutions.ae>
2020-04-04 11:17:58 +0400

    Optimized the translation code

--- app/controllers/user/free_form_widget_controller.rb
+++ app/controllers/user/free_form_widget_controller.rb
@@ -15,7 +15,7 @@ module User
 
       @input_text = params[:text].strip
       @translated_hash = Translation.translate(@input_text, input_language: @input_language, output_language: @output_language, return_in_hash: true)
-
+      
       @display_text = ""
       tokens = @translated_hash["_tokens"]
 
@@ -41,23 +41,8 @@ module User
         end
       end
 
-      # @translated_hash.each do |key, value|
-      #   if value
-      #     @display_text.gsub!(key, value)
-      #   else
-      #     @display_text.gsub!(key, "<span class='its-tran-not-found'><i class=\"icon-question mr-2\"></i>#{key}</span>")
-      #   end
-      # end
-
-      # delimitters = @input_text.scan(Regexp.union(Translation::DELIMITERS))
-
-      # delimitters.each do |dlmtr|
-      #   dlmtr_translations = Translation::DELIMITERS_TRANSLATIONS[dlmtr.to_sym]
-      #   translated_dlmtr = dlmtr_translations.try(:[], @output_language.upcase.to_sym)
-      #   @display_text.gsub!(dlmtr, translated_dlmtr) if translated_dlmtr
-      # end
-
-      @display_text.gsub!("\n", '<br>')
+      @display_text.gsub!(/\n+/, '<br>')
+      
     end
 
     def new_translation_request
