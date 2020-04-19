0159bfeb56ca68d75b7bd126984a8d34102e5223
vanirankar <vanirankar@gmail.com>
2020-04-19 07:41:59 +0530

    Clean

--- app/models/subscription_permission.rb
+++ app/models/subscription_permission.rb
@@ -1,14 +1,11 @@
 class SubscriptionPermission < ApplicationRecord
 
-  # Constants
-  #LANGUAGES = ["ENGLISH", "ARABIC", "FRENCH"].freeze
+ 
 
   # Set Table Name
   self.table_name = "subscription_permissions"
 
- 
-#Association
-  belongs_to :permissions
+
   # Generic Methods
  
   def access
