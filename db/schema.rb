16936419974cfba40d14b14f99dc18c4d9a96c05
sanoop <sanoop.snair@gmail.com>
2020-09-11 15:24:19 +0530

    Update changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_09_09_123307) do
+ActiveRecord::Schema.define(version: 2020_09_09_084301) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -88,17 +87,6 @@ ActiveRecord::Schema.define(version: 2020_09_09_123307) do
     t.index ["unlock_token"], name: "index_client_users_on_unlock_token", unique: true
   end
 
-  create_table "clients_feedbacks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", limit: 256, null: false
-    t.string "type"
-    t.text "input", null: false
-    t.text "output", null: false
-    t.string "remarks"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-
   create_table "document_folders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", limit: 256, null: false
     t.bigint "parent_id"
@@ -109,7 +97,6 @@ ActiveRecord::Schema.define(version: 2020_09_09_123307) do
     t.index ["ancestry"], name: "index_document_folders_on_ancestry"
     t.index ["user_id"], name: "index_document_folders_on_user_id"
   end
-
 
   create_table "identities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "client_user_id"
@@ -263,9 +250,9 @@ ActiveRecord::Schema.define(version: 2020_09_09_123307) do
 
   create_table "uploads_summaries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "translation_uploads_history_id", null: false
+    t.json "summary_new", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.json "summary_new"
   end
 
   create_table "user_subscriptions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
