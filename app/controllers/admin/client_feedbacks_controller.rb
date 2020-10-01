4a8ac2cc5642de024e60824a708dae8706a20b66
sanoop <sanoop.snair@gmail.com>
2020-10-01 15:30:11 +0530

    Complete task

--- app/controllers/admin/client_feedbacks_controller.rb
+++ app/controllers/admin/client_feedbacks_controller.rb
@@ -1,34 +1,36 @@
 module Admin
   class ClientFeedbacksController < Admin::BaseController
     
-   before_action :authenticate_admin_user!
-   before_action :get_user, except: [:new, :create, :index]
-    
-   def index
-    @page_title = "Admin Feedbacks | Admin"
-    @nav = 'admin/client_feedbacks'
-    get_collection
-   end
-   
-   def export_client_feedbacks
-    @feedback = ClientFeedback.all
-    respond_to do |format|
-    format.html
-     format.pdf do
-      render  :pdf => 'file_name',
-              :template => '/admin/client_feedbacks/export_client_feedbacks.pdf.erb',
-              :layout => 'pdf.html.erb'
-     end         
-    end
-   end
+    include TableSettings::ClientFeedbacksTs
 
-   private  
-    def get_collection
-      @order_by = "created_at DESC" unless @order_by
-      @relation = ClientFeedback.all
-      @per_page = 10
-      @client_feedback = @relation.order(@order_by).page(@current_page).per(@per_page)
+    before_action :authenticate_admin_user!
+    before_action :get_user, except: [:new, :create, :index]
+    before_action :configure_client_feedbacks_table_settings
+    
+    def index
+      @page_title = "Admin Feedbacks | Admin"
+      @nav = 'admin/client_feedbacks'
+      get_collection
     end
-  
+   
+    def export_client_feedbacks
+      @feedback = ClientFeedback.all
+      respond_to do |format|
+        format.html
+        format.pdf do
+          render  :pdf => 'file_name',
+                  :template => '/admin/client_feedbacks/export_client_feedbacks.pdf.erb',
+                  :layout => 'pdf.html.erb'
+        end         
+      end
+    end
+
+    private  
+      def get_collection
+        @order_by = "created_at DESC" unless @order_by
+        @relation = ClientFeedback.all
+        @per_page = 10
+        @client_feedback = @relation.order(@order_by).page(@current_page).per(@per_page)
+      end
   end
 end
