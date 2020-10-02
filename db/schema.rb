d2dfd7976f54782b1aa644a4b05c8b6f35998e02
sanoop <sanoop.snair@gmail.com>
2020-10-02 11:30:54 +0530

    Update change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_25_191010) do
+ActiveRecord::Schema.define(version: 2020_10_02_053232) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 2020_09_25_191010) do
     t.text "output", null: false
     t.string "remarks", null: false
     t.string "category"
+    t.string "input_language", null: false
+    t.string "output_language", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["client_user_id"], name: "index_client_feedbacks_on_client_user_id"
@@ -107,15 +109,6 @@ ActiveRecord::Schema.define(version: 2020_09_25_191010) do
     t.string "ancestry"
     t.index ["ancestry"], name: "index_document_folders_on_ancestry"
     t.index ["user_id"], name: "index_document_folders_on_user_id"
-  end
-
-  create_table "folders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "title", limit: 256, null: false
-    t.bigint "parent_id"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_folders_on_user_id"
   end
 
   create_table "identities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -246,6 +239,19 @@ ActiveRecord::Schema.define(version: 2020_09_25_191010) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["client_user_id"], name: "index_translation_query_histories_on_client_user_id"
+  end
+
+  create_table "translation_requests", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "requested_by_id"
+    t.text "input_phrase", null: false
+    t.string "input_language", limit: 16, null: false
+    t.text "output_phrase"
+    t.string "output_language", limit: 16, null: false
+    t.string "doc_type", limit: 256
+    t.string "status", limit: 16, default: "ACTIVE", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["requested_by_id"], name: "index_translation_requests_on_requested_by_id"
   end
 
   create_table "translations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
