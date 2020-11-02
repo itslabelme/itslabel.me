386f4e048094607fcf9438fb6d7f272bb0dd0ed3
asn-athira <athira@rightsolution.ae>
2020-11-02 15:59:03 +0530

    sendgrid fixed

--- app/controllers/admin/client_users_controller.rb
+++ app/controllers/admin/client_users_controller.rb
@@ -12,29 +12,6 @@ module Admin
       new_client_user
     end
 
-    # TODO:- For Mailing
-    # def forgot_password
-    #   get_client_email
-    #   if ClientUser.exists?(email: @email)
-    #     user = ClientUser.find_by(email: @email)
-    #     user.reset_password_token = SecureRandom.urlsafe_base64
-    #     @t=user.reset_password_token
-    #     user.reset_password_sent_at = Time.now.utc
-    #     @s=user.reset_password_sent_at 
-    #     user.save
-    #     UserMailerNotification.send_forgot_password(@email,@t,@s).deliver     
-    #     render 'forgot_password',:layout => false
-    #   else
-    #     set_notification(false, I18n.t('status.error'),"Please correct them and submit again")
-    #     set_flash_message('The form has some errors. Please correct them and submit again', :error) 
-    #   end      
-    # end
-
-    # TODO:- For Mailing
-    # def update_password
-    #   #render 'update_password',:layout => false
-    # end
-
     def show
       get_client_user
     end
@@ -49,12 +26,6 @@ module Admin
       @client_user = ClientUser.find_by(id: params[:id])
     end
 
-    # TODO:- For Mailing
-    # def get_client_email
-    #   params_email = params.require(:client_user).permit(:email)
-    #   @email = params_email[:email]
-    # end
-  
     def get_collection
       @relation = ClientUser.where("")
 
@@ -73,13 +44,6 @@ module Admin
     def client_user_params
       params.require(:client_user).permit(:id, :email, :first_name, :last_name, :mobile_number)
     end
-
-    # TODO:- For Mailing
-    # def get_client_tokens
-    #   @p_token = params[:t]
-    #   @p_time = params[:s]
-      
-    # end
 
   end
 end
