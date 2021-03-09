249974b2b00e85a85e2d4bca68a924d8086d376f
asn-athira <athira@rightsolution.ae>
2021-03-09 10:27:38 +0530

    ui changed

--- app/controllers/admin/translation_uploads_history_controller.rb
+++ app/controllers/admin/translation_uploads_history_controller.rb
@@ -15,33 +15,20 @@ module Admin
       @upload_history = UploadsHistory.find(params[:id])
       # FIX ME - this method needs to be removed. instead use associatoin. 
       # @upload_summary = @upload_history.summary
+      # Select Language
       if params[:filters].nil?
         @input_language = "English"
       else
         @input_language = params[:filters].try(:[], :input_language)
       end
       @upload_summary = UploadsSummary.find_by_translation_uploads_history_id(params[:id])
-      #set_language
   	end
 
   	private
 
-    def apply_filters
-
-      if params[:filters].nil?
-        @input_language = "English"        
-      else  
-        @input_language = params[:filters].try(:[], :input_language)
-      end
-
-      @relation = @relation.search_only_input_language(@input_language)
-
-    end
-
     def get_collection
       @order_by = "created_at DESC" unless @order_by
       @relation = UploadsHistory.where("")
-      #apply_filters
       @upload_histories = @relation.order(@order_by).page(@current_page).per(@per_page)
     end
     
