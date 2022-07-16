169d1c5fbc6e2be167bf84933b34825bfd789dd8
sanoop <sanoop.snair@gmail.com>
2022-07-16 14:55:26 +0530

    update changes

--- app/controllers/api/user_subscriptions_controller.rb
+++ app/controllers/api/user_subscriptions_controller.rb
@@ -30,7 +30,44 @@ module Api
     def test_api
       # puts "------------------ ** Testing API result POST ** -----------------".white
       # puts " Params in Post".white
-      puts params['Parameters']
+      # puts params['Parameters']
+      # puts params
+      # payload = request.body.read
+
+
+      # Its working------>>
+      # payload = Hash.from_xml(request.body.read)
+      # payload = Hash.from_xml(response).to_json
+      # puts payload
+
+      # out_hash = Hash.from_xml(request.body.read)
+      out_hash = Hash.from_xml(request.body.read.gsub("\n", "").gsub("\n", ""))
+      # out_hash = Hash.from_xml(request.body.read.gsub("\n", ""))
+      puts out_hash
+      # puts out_hash.Parameters
+
+
+      # payload = request.body.read
+      # sig_header = request.env['HTTP_STRIPE_SIGNATURE']
+      # event = nil
+
+      # begin
+      #   event = Stripe::Webhook.construct_event(
+      #     payload, sig_header, Rails.application.credentials[:stripe][:webhook]
+      #   )
+      # rescue JSON::ParserError => e
+      #   status 400
+      #   return
+      # rescue Stripe::SignatureVerificationError => e
+      #   # Invalid signature
+      #   puts "Signature error"
+      #   p e
+      #   return
+      # end
+      # puts event
+
+
+
       # puts params['subscription'].to_json
       render json: {"status": "Done"}
 
