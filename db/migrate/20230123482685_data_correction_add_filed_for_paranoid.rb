f1411d7b41957fa4e5d24064151f577b6026ab50
sanoop <sanoop.snair@gmail.com>
2023-02-01 12:34:21 +0530

    Update changes

--- db/migrate/20230123482685_data_correction_add_filed_for_paranoid.rb
+++ db/migrate/20230123482685_data_correction_add_filed_for_paranoid.rb
@@ -1,6 +1,6 @@
 class DataCorrectionAddFiledForParanoid < ActiveRecord::Migration[5.2]
   def change
-    # add_column :client_users, :deleted_at, :datetime
+    add_column :client_users, :deleted_at, :datetime
     add_index :client_users, :deleted_at
   end
 end
