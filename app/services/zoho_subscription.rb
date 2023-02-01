f6c887467c9fe32bb48c2dfa33f2858125193be0
sanoop <sanoop.snair@gmail.com>
2023-02-01 13:51:14 +0530

    update changes

--- app/services/zoho_subscription.rb
+++ app/services/zoho_subscription.rb
@@ -102,12 +102,13 @@ class ZohoSubscription
 
     if access_token
           # subscription data
-        # subscription_data = {customer: {display_name: @params[:display_name], first_name: @params[:first_name], last_name: @params[:last_name], email: @params[:email], company_name: @params[:company_name], currency_code: 'USD'}, plan: {name: "Free Plan", plan_code: "Free", plan_description: "Free Plan", price: 0, quantity: 1}, auto_collect: false}
-        subscription_data = {pricebook_id: Rails.application.secrets.zoho_pricebook_id, customer: {display_name: @params[:display_name], first_name: @params[:first_name], last_name: @params[:last_name], email: @params[:email], company_name: @params[:company_name], currency_code: 'USD'}, plan: {name: "Free Plan", plan_code: "Free", plan_description: "Free Plan", price: 0, quantity: 1}, auto_collect: false}
+        subscription_data = {customer: {display_name: @params[:display_name], first_name: @params[:first_name], last_name: @params[:last_name], email: @params[:email], company_name: @params[:company_name], currency_code: 'USD'}, plan: {name: "Free Plan", plan_code: "Free", plan_description: "Free Plan", price: 0, quantity: 1}, auto_collect: false}
+        # subscription_data = {pricebook_id: '3630804000000089048', customer: {display_name: @params[:display_name], first_name: @params[:first_name], last_name: @params[:last_name], email: @params[:email], company_name: @params[:company_name], currency_code: 'USD'}, plan: {name: "Free Plan", plan_code: "Free", plan_description: "Free Plan", price: 0, quantity: 1}, auto_collect: false}
 
         begin
            # Create the HTTP request
           uri = URI("https://subscriptions.zoho.com/api/v1/subscriptions?organization_id=#{Rails.application.secrets.zoho_organization_id}")
+          # uri = URI("https://subscriptions.zoho.com/api/v1/subscriptions?organization_id=794958424")
           req = Net::HTTP::Post.new(uri)
           req["Content-Type"] = "application/json"
           req["Authorization"] = "Zoho-oauthtoken #{access_token}"
